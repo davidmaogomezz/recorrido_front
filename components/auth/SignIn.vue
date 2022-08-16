91d2c432aa95f3656f84ab3583fe9a7ed8e3b377
David Mauricio Gomez Zapata <davidmaogomezz@gmail.com>
2022-08-16 12:06:48 -0500

    Add vuelidate

--- components/auth/SignIn.vue
+++ components/auth/SignIn.vue
@@ -1,56 +1,88 @@
 <template>
   <div class="container sign-in">
     <h1 class="title-sign-in">Iniciar sesión</h1>
-    <label for="email">Email</label>
-    <b-form-input class="input-auth" v-model="login.email" type="email"/>
-    <label for="password">Password</label>
-    <b-form-input class="input-auth" v-model="login.password" type="password"/>
-    <b-button @click.prevent="userLogin" block variant="outline-primary">Ingresar</b-button>
+    <form @submit.prevent="handleSubmit">
+      <label for="email">Email</label>
+      <b-form-input class="input-auth" v-model="user.email" type="email" />
+      <div v-if="$v.user.email.$invalid">
+        <p class="message-error">{{ 'email requerido' }}</p>
+      </div>
+      <label for="password">Password</label>
+      <b-form-input class="input-auth" v-model="user.password" type="password" />
+      <div v-if="$v.user.password.$invalid">
+        <p class="message-error">{{ `password requerido, mínimo ${$v.user.password.$params.minLength.min} caracteres` }}</p>
+      </div>
+      <b-button block variant="outline-primary" type="submit">Ingresar</b-button>
+    </form>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        login: {
-          email: 'admin@example.com',
-          password: 'password'
-        }
+import { required, email, minLength } from "vuelidate/lib/validators";
+export default {
+  data() {
+    return {
+      user: {
+        email: 'admin@mail.com',
+        password: '12345678'
+      },
+      submitted: false
+    }
+  },
+  validations: {
+    user: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(8)
       }
-    },
-    methods: {
-      async userLogin() {
-        try {
-          const request = await this.$axios.post('users/sign_in', { user: this.login })
-          if (request.status == 200) {
-            const headers = request.headers
-            this.$cookies.set('auth', true, {})
-            this.$cookies.set('accessToken', headers['access-token'], {})
-            this.$cookies.set('client', headers['client'], {})
-            this.$cookies.set('uid', headers['uid'], {})
-            this.$cookies.set('role', request.data.user.role, {})
-            this.$router.push('/auth')
-          }
-        } catch (error) {
-          console.log(`error -> ${error}`)
-        }
+    }
+  },
+  methods: {
+    async handleSubmit() {
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        return;
       }
-    },
-  }
+      try {
+        const request = await this.$axios.post('users/sign_in', { user: this.user })
+        if (request.status == 200) {
+          const headers = request.headers
+          this.$cookies.set('auth', true, {})
+          this.$cookies.set('accessToken', headers['access-token'], {})
+          this.$cookies.set('client', headers['client'], {})
+          this.$cookies.set('uid', headers['uid'], {})
+          this.$cookies.set('role', request.data.user.role, {})
+          this.$router.push('/auth')
+        }
+      } catch (error) {
+        console.log(`error -> ${error}`)
+      }
+    }
+  },
+}
 </script>
 
 <style scoped>
-  .sign-in {
-    margin-top: 20px;
-  }
+.sign-in {
+  margin-top: 20px;
+}
 
-  .input-auth {
-    margin-bottom: 10px;
-  }
-  .title-sign-in {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.input-auth {
+  margin-bottom: 10px;
+}
+
+.title-sign-in {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.message-error {
+  font-size: 12px;
+  color: red;
+}
 </style>
--- components/auth/SignUp.vue
+++ components/auth/SignUp.vue
@@ -2,33 +2,54 @@
   <div class="container sign-up">
     <h1 class="title-sign-up">Registrarme</h1>
     <p v-if="message != ''">{{ message }}</p>
-    <label for="first_name">First name</label>
-    <b-form-input name="first_name" class="input-auth" v-model="register.first_name" type="text" />
-    <label for="last_name">last_name</label>
-    <b-form-input name="last_name" class="input-auth" v-model="register.last_name" type="text" />
-    <label for="user_name">UserName</label>
-    <b-form-input name="user_name" class="input-auth" v-model="register.username" type="text" />
-    <label for="email">Email</label>
-    <b-form-input name="email" class="input-auth" v-model="register.email" type="email" />
-    <label for="role">Rol:</label>
-    <b-form-select v-model="register.role" :options="optionsRole"></b-form-select>
-    <label for="password">Password</label>
-    <b-form-input name="password" class="input-auth" v-model="register.password" type="password" />
-    <b-button @click.prevent="userRegister" block variant="outline-primary">Ingresar</b-button>
+    <form @submit.prevent="handleSubmit">
+      <label for="first_name">First name</label>
+      <b-form-input name="first_name" class="input-auth" v-model="user.first_name" type="text" />
+      <div v-if="$v.user.first_name.$invalid">
+        <p class="message-error">{{ '* requerido' }}</p>
+      </div>
+      <label for="last_name">last_name</label>
+      <b-form-input name="last_name" class="input-auth" v-model="user.last_name" type="text" />
+      <div v-if="$v.user.last_name.$invalid">
+        <p class="message-error">{{ '* requerido' }}</p>
+      </div>
+      <label for="user_name">Username</label>
+      <b-form-input name="user_name" class="input-auth" v-model="user.username" type="text" />
+      <div v-if="$v.user.username.$invalid">
+        <p class="message-error">{{ '* requerido' }}</p>
+      </div>
+      <label for="email">Email</label>
+      <b-form-input name="email" class="input-auth" v-model="user.email" type="email" />
+      <div v-if="$v.user.email.$invalid">
+        <p class="message-error">{{ 'email requerido' }}</p>
+      </div>
+      <label for="role">Rol:</label>
+      <b-form-select v-model="user.role" :options="optionsRole"></b-form-select>
+      <div v-if="$v.user.role.$invalid">
+        <p class="message-error">{{ '* requerido' }}</p>
+      </div>
+      <label for="password">Password</label>
+      <b-form-input name="password" class="input-auth" v-model="user.password" type="password" />
+      <div v-if="$v.user.password.$invalid">
+        <p class="message-error">{{ `Campo password requerido, minimo ${$v.user.password.$params.minLength.min} caracteres` }}</p>
+      </div>
+      <b-button block variant="outline-primary" type="submit">Ingresar</b-button>
+    </form>
   </div>
 </template>
 
 <script>
+  import { required, email, minLength } from "vuelidate/lib/validators";
   export default {
     data() {
       return {
-        register: {
+        user: {
           first_name: 'David',
           last_name: 'Gómez',
           username: 'davidmao',
-          email: 'admin@example.com',
-          password: 'password',
-          role: 0
+          email: 'admin@mail.com',
+          password: '12345678',
+          role: 0,
         },
         message: '',
         optionsRole: [
@@ -47,10 +68,38 @@
         ]
       }
     },
+    validations: {
+      user: {
+        first_name: {
+          required
+        },
+        last_name: {
+          required
+        },
+        username: {
+          required
+        },
+        email: {
+          required,
+          email
+        },
+        password: {
+          required,
+          minLength: minLength(8)
+        },
+        role: {
+          required
+        }
+      }
+    },
     methods: {
-      async userRegister() {
+      async handleSubmit() {
+        this.$v.$touch();
+        if (this.$v.$invalid) {
+          return;
+        }
         try {
-          const request = await this.$axios.post('users', { user: this.register })
+          const request = await this.$axios.post('users', { user: this.user })
           if (request.status == 200) {
             const headers = request.headers
             this.$cookies.set('auth', true, {})
@@ -80,5 +129,10 @@
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+
+  .message-error {
+    font-size: 12px;
+    color: red;
   }
 </style>
