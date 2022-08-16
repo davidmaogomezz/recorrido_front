f183feaaea5a0da4fad8f6d264b8ae00b644b87e
David Mauricio Gomez Zapata <davidmaogomezz@gmail.com>
2022-08-16 16:09:52 -0500

    create method fill weeks for drodown

--- components/auth/expert/index.vue
+++ components/auth/expert/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Component expert</h1>
+    <p>Weeks {{JSON.stringify(weeks)}}</p>
   </div>
 </template>
 
@@ -8,9 +9,29 @@
   export default {
     layout: 'auth',
     middleware: ['protect'],
+    methods: {
+      fillWeeks() {
+        const currentWeek = this.$moment().format('W')
+        const currentYear = this.$moment().format('YYYY')
+        let count = 0
+        while (count < 6) {
+          let week = parseInt(currentWeek) + count
+          let elementDropdown = {
+            text: `Semana ${week} ${currentYear}`,
+            value: week
+          }
+          this.weeks.push(elementDropdown)
+          count++
+        }
+      }
+    },
     data() {
       return {
+        weeks: []
       }
+    },
+    mounted() {
+      this.fillWeeks()
     }
   }
 </script>
