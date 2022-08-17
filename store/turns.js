0618d8f0ce433c15e8ef535f31843ee39e368721
David Mauricio Gomez Zapata <davidmaogomezz@gmail.com>
2022-08-17 17:01:09 -0500

    create action and mutation update turn

--- store/turns.js
+++ store/turns.js
@@ -5,12 +5,28 @@ export const state = () => ({
 export const mutations = {
   STORE_TURNS (state, turns) {
     state.turns = turns
+  },
+  UPDATE_TURNS (state, turn) {
+    const date_hour_split = turn.date_hour.split('-')
+    const day = date_hour_split[2].split('T')[0]
+    const date = `${day}/${date_hour_split[1]}/${date_hour_split[0]}`
+    let turns = state.turns[date]
+    debugger
+    if (turns) {
+      turns = turns.filter(t => t.id !== turn.id)
+      turns.push(turn)
+      turns.sort((a, b) => a.id - b.id)
+      state.turns[date] = turns
+    }
   }
 }
 
 export const actions = {
   storeTurns({commit}, turns) {
     commit('STORE_TURNS', turns)
+  },
+  updateTurn({commit}, turn) {
+    commit('UPDATE_TURNS', turn)
   }
 }
 
